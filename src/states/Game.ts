ca422a2dd748eeb4301506460c09bb45ca8327f4
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-01-13 09:29:10 -0500

    Create new platforms from array

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,14 +1,22 @@
 import { Platform } from '../prefabs/Platform';
 
 export class Game extends Phaser.State {
+  private currentIndex: number;
+  private currentPlatform: Platform;
   private cursor: Phaser.CursorKeys;
   private floorPool: Phaser.Group;
   private isJumping: boolean;
   private jumpPeaked: boolean;
+  private levelData: {
+    platforms: Array<{
+      numTiles: number,
+      seperation: number,
+      y: number,
+    }>,
+  };
   private readonly maxJumpDistance = 120;
   private myCoins = 0;
   private player: Phaser.Sprite;
-  private platform: Platform;
   private platformPool: Phaser.Group;
   private startJumpY: number;
   private levelSpeed = 200;
@@ -30,12 +38,13 @@ export class Game extends Phaser.State {
     (this.player.body as Phaser.Physics.Arcade.Body).setSize(38, 60, 7, 4);
     this.player.play('running');
 
-    this.platform = new Platform(this.game, this.floorPool, 12, 0, 200, -this.levelSpeed);
-    this.platformPool.add(this.platform);
+    // this.currentPlatform = new Platform(this.game, this.floorPool, 12, 0, 200, -this.levelSpeed);
+    // this.platformPool.add(this.currentPlatform);
+    this.loadLevel();
   }
 
   public update() {
-    this.platform.forEachAlive((platform: Phaser.Group, index: number) => {
+    this.currentPlatform.forEachAlive((platform: Phaser.Group, index: number) => {
       this.physics.arcade.collide(this.player, platform);
     }, this);
 
@@ -50,11 +59,79 @@ export class Game extends Phaser.State {
     } else if (this.cursor.up.isUp || this.input.activePointer.isUp) {
       this.isJumping = false;
     }
+
+    if (this.currentPlatform.length &&
+        (this.currentPlatform.children[this.currentPlatform.length - 1] as Phaser.Sprite).right < this.world.width) {
+      this.createPlatform();
+    }
   }
 
   public render() {
     // this.game.debug.body(this.player);
     // this.game.debug.bodyInfo(this.player, 0, 30);
+  }
+
+  private createPlatform() {
+    const nextPlatformData = this.levelData.platforms[this.currentIndex];
+
+    if (nextPlatformData) {
+      this.currentPlatform = new Platform(this.game,
+        this.floorPool,
+        nextPlatformData.numTiles,
+        this.world.width + nextPlatformData.seperation,
+        nextPlatformData.y,
+        -this.levelSpeed);
+
+      this.platformPool.add(this.currentPlatform);
+
+      this.currentIndex++;
+    }
+  }
+
+  private loadLevel() {
+    this.levelData = {
+      platforms: [
+        {
+          numTiles: 4,
+          seperation: 50,
+          y: 200,
+        },
+        {
+          numTiles: 6,
+          seperation: 50,
+          y: 250,
+        },
+        {
+          numTiles: 3,
+          seperation: 100,
+          y: 200,
+        },
+        {
+          numTiles: 8,
+          seperation: 50,
+          y: 250,
+        },
+        {
+          numTiles: 10,
+          seperation: 100,
+          y: 200,
+        },
+        {
+          numTiles: 4,
+          seperation: 100,
+          y: 300,
+        },
+        {
+          numTiles: 4,
+          seperation: 50,
+          y: 200,
+        },
+      ],
+    };
+
+    this.currentIndex = 0;
+
+    this.createPlatform();
   }
 
   private playerJump() {
@@ -65,7 +142,7 @@ export class Game extends Phaser.State {
       (this.player.body as Phaser.Physics.Arcade.Body).velocity.y = -300;
     } else if (this.isJumping && !this.jumpPeaked) {
       const distanceJumped = this.startJumpY - this.player.y;
-      console.log('distanceJumped');
+
       if (distanceJumped <= this.maxJumpDistance) {
         (this.player.body as Phaser.Physics.Arcade.Body).velocity.y = -300;
       } else {
